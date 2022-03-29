56139c88c0d36e4c8ce9f38bcefe2752d637c829
Kecar2 <kendrycarvajalsegura>
2022-03-29 14:07:08 +0200

    actualizado

--- scripts/dom.js
+++ scripts/dom.js
@@ -10,5 +10,28 @@ let DOM = {
             span.textContent = nextLetter;
             document.querySelector("#puzzle").appendChild(span);
         }
+
+        //console.log(guessedWord);
+    },
+    addGuessedLetter(letter, isCorrect) {
+        const span = document.createElement("span");
+        span.textContent = letter;
+        span.style.color = (isCorrect) ? 'green' : 'red';
+        document.querySelector("#letters-tried").appendChild(span);
+
+    },
+
+    updateAttempts(attempts) {
+        document.querySelector("#guesses span").textContent = attempts;
+    },
+
+    showLooserMessage() {
+        document.querySelector("#guesses").innerHTML = `
+        <span style="color:red;width: auto;">¡Has perdido! </span>`;
+    },
+
+    showWinnerMessage() {
+        document.querySelector("#guesses").innerHTML = `
+        <span style="color:green;width: auto;">¡Has ganado! </span>`;
     }
 };
--- scripts/state.js
+++ scripts/state.js
@@ -1,9 +1,15 @@
 let STATE = {
     movie: undefined,
     movieGuess: undefined,
+    checkedLetters: [],
+    attempts: 5,
+    GAME_STATUS: 0,
 
     reset(movie) {
         this.movie = movie;
         this.movieGuess = movie.replaceAll(/[a-zA-z]/g, "*");
+        this.checkedLetters = [];
+        this.attempts = 5;
+        this.GAME_STATUS = 0;
     }
 };
